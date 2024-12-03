56baa7740c8cae97568ba4f2994acb606f68b7ed
a <a>
2024-12-03 19:17:16 +0300

    qww

--- lua/plugins/ui/dashboard.lua
+++ lua/plugins/ui/dashboard.lua
@@ -1,102 +1,39 @@
 return {
-  {
-    "nvimdev/dashboard-nvim",
-    enabled = false,
-    lazy = false,
-    opts = function()
-      local logo = require("plugins.ui.logos.026")
+  "nvimdev/dashboard-nvim",
+  enabled = true,
+  lazy = false,
+  opts = function()
+    local logo = require("plugins.ui.logos.026")
 
-      local opts = {
-        theme = "doom",
-        hide = {
-          statusline = false,
-        },
-        config = {
-          header = vim.split(logo, "\n"),
-          center = {
-            { action = "lua LazyVim.pick()()", desc = " Hайти файл", icon = " ", key = "f" },
-            { action = "ene | startinsert", desc = " Hовый файл", icon = " ", key = "n" },
-            {
-              action = 'lua LazyVim.pick("oldfiles")()',
-              desc = " Hедавние файлы",
-              icon = " ",
-              key = "r",
-            },
-            { action = 'lua LazyVim.pick("live_grep")()', desc = " Hайти текст", icon = " ", key = "g" },
-            { action = "lua LazyVim.pick.config_files()()", desc = " Kонфиг", icon = " ", key = "c" },
-            {
-              action = 'lua require("persistence").load()',
-              desc = " Bосстановить сессию",
-              icon = " ",
-              key = "s",
-            },
-            {
-              action = function()
-                vim.api.nvim_input("<cmd>qa<cr>")
-              end,
-              desc = " Bыход",
-              icon = " ",
-              key = "q",
-            },
+    local opts = {
+      theme = "doom",
+      hide = {
+        statusline = false,
+      },
+      config = {
+        header = vim.split(logo, "\n"),
+        center = {
+          { action = "lua LazyVim.pick()()", desc = " Hайти файл", icon = " ", key = "f" },
+          { action = "ene | startinsert", desc = " Hовый файл", icon = " ", key = "n" },
+          { action = 'lua LazyVim.pick("oldfiles")()', desc = " Hедавние файлы", icon = " ", key = "r" },
+          { action = 'lua LazyVim.pick("live_grep")()', desc = " Hайти текст", icon = " ", key = "g" },
+          { action = "lua LazyVim.pick.config_files()()", desc = " Kонфиг", icon = " ", key = "c" },
+          {
+            action = 'lua require("persistence").load()',
+            desc = " Bосстановить сессию",
+            icon = " ",
+            key = "s",
           },
-          footer = function()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-          end,
-        },
-      }
-
-      for _, button in ipairs(opts.config.center) do
-        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-        button.key_format = "  %s"
-      end
-
-      -- open dashboard after closing lazy
-      if vim.o.filetype == "lazy" then
-        vim.api.nvim_create_autocmd("WinClosed", {
-          pattern = tostring(vim.api.nvim_get_current_win()),
-          once = true,
-          callback = function()
-            vim.schedule(function()
-              vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
-            end)
-          end,
-        })
-      end
-
-      return opts
-    end,
-  },
-  {
-    "folke/snacks.nvim",
-    enabled = true,
-    opts = {
-      dashboard = {
-        preset = {
-          header = [[
-        ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
-        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
-        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
-        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
-        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
- ]],
-          -- stylua: ignore
-          ---@type snacks.dashboard.Item[]
-          keys = {
-            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          {
+            action = function()
+              vim.api.nvim_input("<cmd>qa<cr>")
+            end,
+            desc = " Bыход",
+            icon = " ",
+            key = "q",
           },
         },
       },
-    },
-  },
+    }
+  end,
 }
--- lua/plugins/ui/profile.lua
+++ lua/plugins/ui/profile.lua
@@ -1,50 +1,50 @@
 return {
-  "Kurama622/profile.nvim",
-  dependencies = { "3rd/image.nvim" },
-  config = function()
-    local comp = require("profile.components")
-    require("profile").setup({
+  {
+    "Kurama622/profile.nvim",
+    enabled = false,
+    dependencies = { "3rd/image.nvim" },
+    config = function()
+      local comp = require("profile.components")
+      require("profile").setup({
 
-      avatar_path =
-      "/home/username/.config/nvim/lua/plugins/ui/pictures/devushka_ekaj_stoit_nochu_pri_svete_luni_na.png",
-      avatar_opts = {
-        avatar_width = 150,
-        avatar_height = 150,
-        avatar_x = 60,
-        avatar_y = 20,
-        force_blank = false,
-      },
+        avatar_path = "/home/username/.config/nvim/lua/plugins/ui/pictures/devushka_ekaj_stoit_nochu_pri_svete_luni_na.png",
+        avatar_opts = {
+          avatar_width = 150,
+          avatar_height = 150,
+          avatar_x = 60,
+          avatar_y = 20,
+          force_blank = false,
+        },
 
-      git_contributions = {
-        start_week = 1,
-        end_week = 53,
-        empty_char = " ",
-        full_char = { "", "󰧞", "", "", "" },
-        fake_contributions = nil,
-        cache_path = "/tmp/profile.nvim/",
-        cache_duration = 24 * 60 * 60,
+        git_contributions = {
+          start_week = 1,
+          end_week = 53,
+          empty_char = " ",
+          full_char = { "", "󰧞", "", "", "" },
+          fake_contributions = nil,
+          cache_path = "/tmp/profile.nvim/",
+          cache_duration = 24 * 60 * 60,
+        },
+        hide = {
+          statusline = false,
+          tabline = false,
+        },
+        disable_keys = { "h", "j", "k", "<Left>", "<Right>", "<Up>", "<Down>", "<C-f>" },
+        format = function()
+          comp:avatar()
+          comp:text_component_render({
+            -- comp:text_component("", "center", "ProfileRed"),
+            -- comp:text_component("──── By Kurama622", "right", "ProfileBlue"),
+          })
+          comp:separator_render()
 
-      },
-      hide = {
-        statusline = false,
-        tabline = false,
-      },
-      disable_keys = { "h", "j", "k", "<Left>", "<Right>", "<Up>", "<Down>", "<C-f>" },
-      format = function()
-        comp:avatar()
-        comp:text_component_render({
-          -- comp:text_component("", "center", "ProfileRed"),
-          -- comp:text_component("──── By Kurama622", "right", "ProfileBlue"),
-        })
-        comp:separator_render()
-
-        comp:card_component_render({
-          type = "table",
-          content = function()
-            return {
-              {
-                title = "Files",
-                description = [[
+          comp:card_component_render({
+            type = "table",
+            content = function()
+              return {
+                {
+                  title = "Files",
+                  description = [[
 C -> Neovim
 H -> Hyprland
 M -> Notes
@@ -52,51 +52,58 @@ R -> Recently
 F -> Find files
 N -> New file
 ]],
-              },
-              {
-                title = "Session",
-                description = [[
+                },
+                {
+                  title = "Session",
+                  description = [[
 Q -> Quit
 S -> Restore session
 L -> Lazy
 ]],
-              },
-            }
-          end,
-          hl = {
-            border = "ProfileWhite",
-            text = "ProfileWhite",
-          },
-        })
-        comp:separator_render()
+                },
+              }
+            end,
+            hl = {
+              border = "ProfileWhite",
+              text = "ProfileWhite",
+            },
+          })
+          comp:separator_render()
 
-        comp:git_contributions_render("ProfileGreen")
-      end,
-    })
-    vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>Profile<cr>", { silent = true })
-    local user_mappings = {
-      n = {
-        ["r"] = "<cmd>lua require('telescope.builtin').oldfiles()<cr>",
-        ["f"] = "lua LazyVim.pick()()",
-        ["c"] = "<cmd>lua require('telescope.builtin').find_files({ cwd = '$HOME/.config/nvim' })<cr>",
-        ["h"] = "<cmd>lua require('telescope.builtin').find_files({ cwd = '$HOME/.config/hypr/' })<cr>",
-        ["m"] = "<cmd>lua require('telescope.builtin').find_files({ cwd = '$HOME/Documents/Notes/' })<cr>",
-        ["g"] = "<cmd>lua require('telescope.builtin').live_grep()<cr>",
-        ["s"] = "<cmd>lua require('persistence').load()<cr>",
-        ["q"] = "<cmd>q<cr>",
-        ["n"] = "ene | startinsert",
-        ["l"] = "<cmd>Lazy<cr>",
-      },
-    }
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "profile",
-      callback = function()
-        for mode, mapping in pairs(user_mappings) do
-          for key, cmd in pairs(mapping) do
-            vim.api.nvim_buf_set_keymap(0, mode, key, cmd, { noremap = true, silent = true })
+          comp:git_contributions_render("ProfileGreen")
+        end,
+      })
+      vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>Profile<cr>", { silent = true })
+      local user_mappings = {
+        n = {
+          ["r"] = "<cmd>lua require('telescope.builtin').oldfiles()<cr>",
+          ["f"] = "lua LazyVim.pick()()",
+          ["c"] = "<cmd>lua require('telescope.builtin').find_files({ cwd = '$HOME/.config/nvim' })<cr>",
+          ["h"] = "<cmd>lua require('telescope.builtin').find_files({ cwd = '$HOME/.config/hypr/' })<cr>",
+          ["m"] = "<cmd>lua require('telescope.builtin').find_files({ cwd = '$HOME/Documents/Notes/' })<cr>",
+          ["g"] = "<cmd>lua require('telescope.builtin').live_grep()<cr>",
+          ["s"] = "<cmd>lua require('persistence').load()<cr>",
+          ["q"] = "<cmd>q<cr>",
+          ["n"] = "ene | startinsert",
+          ["l"] = "<cmd>Lazy<cr>",
+        },
+      }
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "profile",
+        callback = function()
+          for mode, mapping in pairs(user_mappings) do
+            for key, cmd in pairs(mapping) do
+              vim.api.nvim_buf_set_keymap(0, mode, key, cmd, { noremap = true, silent = true })
+            end
           end
-        end
-      end,
-    })
-  end,
+        end,
+      })
+    end,
+  },
+  {
+    "3rd/image.nvim",
+    enabled = false,
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {},
+  },
 }
