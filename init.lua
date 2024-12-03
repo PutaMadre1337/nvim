75eaae17d1c798d983b70a4922242875fffdf650
a <a>
2024-12-03 16:25:35 +0300

    pisun 2

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
 require("config.lazy")
+
+require("lazy").setup({
+  {
+    "3rd/image.nvim",
+    opts = {},
+  },
+}, {
+  rocks = {
+    hererocks = true, -- recommended if you do not have global installation of Lua 5.1.
+  },
+})
