8f21af2ba7a5c753bed1e4a49ff2b526600cf82f
a <a>
2025-01-04 15:45:23 +0300

    tammy completes

--- lua/config/user-commands.lua
+++ lua/config/user-commands.lua
@@ -44,4 +44,7 @@ vim.api.nvim_create_user_command("Tammy", function(args)
   vim.bo.filetype = "man"
 end, {
   nargs = "*",
+  complete = function()
+    return { "-f list", "-f tree", "-ft ", "-h" }
+  end,
 })
