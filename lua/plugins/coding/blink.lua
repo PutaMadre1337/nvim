63a8a64ee57b3d169a20ff2894c10a50711796cd
nightvalley <a>
2025-01-10 11:34:25 +0300

    add colorful-menu for blink

--- lua/plugins/coding/blink.lua
+++ lua/plugins/coding/blink.lua
@@ -44,6 +44,34 @@ return {
         border = borderType,
         draw = {
           treesitter = { "lsp" },
+
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            label = {
+              width = { fill = true, max = 60 },
+              text = function(ctx)
+                local highlights_info = require("colorful-menu").blink_highlights(ctx)
+                if highlights_info ~= nil then
+                  -- Or you want to add more item to label
+                  return highlights_info.label
+                else
+                  return ctx.label
+                end
+              end,
+              highlight = function(ctx)
+                local highlights = {}
+                local highlights_info = require("colorful-menu").blink_highlights(ctx)
+                if highlights_info ~= nil then
+                  highlights = highlights_info.highlights
+                end
+                for _, idx in ipairs(ctx.label_matched_indices) do
+                  table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+                end
+                -- Do something else
+                return highlights
+              end,
+            },
+          },
         },
       },
       documentation = {
