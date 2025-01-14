a1aa44ab5befea8aa11d65526ba338a891eeb2e9
nightvalley <a>
2025-01-14 04:19:50 +0300

    md provider

--- lua/plugins/coding/blink.lua
+++ lua/plugins/coding/blink.lua
@@ -89,7 +89,14 @@ return {
     sources = {
 
       compat = {},
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "markdown" },
+      providers = {
+        markdown = {
+          name = "RenderMarkdown",
+          module = "render-markdown.integ.blink",
+          fallbacks = { "lsp" },
+        },
+      },
       cmdline = {},
     },
 
