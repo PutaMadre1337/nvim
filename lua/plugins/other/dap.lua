def0842227f4cee6e4a7694f46c699caa49871e4
nightvalley <barges_wilts.87@icloud.com>
2025-02-13 10:02:02 +0300

    dap for rust

---
+++ lua/plugins/other/dap.lua
@@ -0,0 +1,18 @@
+return {
+  "mfussenegger/nvim-dap",
+  lazy = true,
+  ft = "rs",
+  dependencies = {
+    {
+      "rcarriga/nvim-dap-ui",
+      config = function(_, opts)
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dapui.setup(opts)
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+          dapui.open({})
+        end
+      end,
+    },
+  },
+}
