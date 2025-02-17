24122aa593486548bbdf49e2b5dcb62792d6ebde
nightvalley <barges_wilts.87@icloud.com>
2025-02-17 07:59:56 +0300

    disable lsp lines

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,42 +1,42 @@
-local autocmd = vim.api.nvim_create_autocmd
-local diag_conf = vim.diagnostic.config
-local lsp_lines = require("lsp_lines")
-
-lsp_lines.setup()
-diag_conf({
-  virtual_text = true,
-  virtual_lines = false,
-}, require("lazy.core.config").ns)
-local function update_diagnostics()
-  diag_conf({
-    virtual_text = {
-      format = function(diagnostic)
-        local win_type = vim.fn.win_gettype()
-        local cur_line = vim.fn.line(".")
-        if win_type == "popup" or win_type == "floating" then
-          return diagnostic.message
-        elseif diagnostic.lnum == cur_line - 1 then
-          --- @diagnostic disable-next-line: return-type-mismatch
-          return nil
-        end
-        return diagnostic.message
-      end,
-    },
-    virtual_lines = { only_current_line = true },
-  })
-end
-update_diagnostics()
-autocmd("CursorMoved", {
-  callback = function()
-    local win_type = vim.fn.win_gettype()
-
-    if win_type == "popup" or win_type == "floating" then
-      return
-    end
-
-    update_diagnostics()
-  end,
-})
+-- local autocmd = vim.api.nvim_create_autocmd
+-- local diag_conf = vim.diagnostic.config
+-- local lsp_lines = require("lsp_lines")
+--
+-- lsp_lines.setup()
+-- diag_conf({
+--   virtual_text = true,
+--   virtual_lines = false,
+-- }, require("lazy.core.config").ns)
+-- local function update_diagnostics()
+--   diag_conf({
+--     virtual_text = {
+--       format = function(diagnostic)
+--         local win_type = vim.fn.win_gettype()
+--         local cur_line = vim.fn.line(".")
+--         if win_type == "popup" or win_type == "floating" then
+--           return diagnostic.message
+--         elseif diagnostic.lnum == cur_line - 1 then
+--           --- @diagnostic disable-next-line: return-type-mismatch
+--           return nil
+--         end
+--         return diagnostic.message
+--       end,
+--     },
+--     virtual_lines = { only_current_line = true },
+--   })
+-- end
+-- update_diagnostics()
+-- autocmd("CursorMoved", {
+--   callback = function()
+--     local win_type = vim.fn.win_gettype()
+--
+--     if win_type == "popup" or win_type == "floating" then
+--       return
+--     end
+--
+--     update_diagnostics()
+--   end,
+-- })
 
 -- This file is automatically loaded by lazyvim.config.init.
 
